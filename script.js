4697f8024085bbe0170ab9b60cc145a947167f8b
umar-rasheed <umar-rasheed@outlook.com>
2026-07-02 00:36:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function toggleEvent(card) {
-    const body = card.querySelector('.event-body');
-    const arrow = card.querySelector('.arrow');
-    
-    body.classList.toggle('show');
-    arrow.style.transform = body.classList.contains('show') ? 'rotate(180deg)' : 'rotate(0)';
-}
-
-// Open the invitation
-document.getElementById('closed-card').addEventListener('click', function() {
-    this.style.display = 'none';
-    document.getElementById('open-content').classList.remove('hidden');
+document.querySelector('.share').addEventListener('click', () => {
+    alert('Invitation link copied! Share with your loved ones.');
 });
